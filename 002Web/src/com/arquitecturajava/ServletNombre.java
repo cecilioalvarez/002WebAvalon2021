0ba17d2eacbe75a1c755220555fcd35a22c924f1
cecilioAlvarez <cmac@arquitecturajava.com>
2021-07-23 12:24:45 +0200

    primeros ejemplos de servlets

---
+++ 002Web/src/com/arquitecturajava/ServletNombre.java
@@ -0,0 +1,30 @@
+package com.arquitecturajava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletNombre
+ */
+@WebServlet("/ServletNombre")
+public class ServletNombre extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nombre=request.getParameter("nombre");
+		PrintWriter pw= response.getWriter();
+		pw.println("hola"+ nombre);
+	}
+
+
+
+}
