db25dfd2b1f23a890f326bdde682f1a4c3efd834
Molocale <molocale@gmail.com>
2021-07-23 13:47:05 +0200

    Tabla de multiplicar

---
+++ 002Web/src/arquitecturajava/ServletTablaMultiplicar.java
@@ -0,0 +1,38 @@
+package arquitecturajava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTablaMultiplicar
+ */
+@WebServlet("/ServletTablaMultiplicar")
+public class ServletTablaMultiplicar extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int number = 0;
+
+		if (request.getParameter("number").chars().allMatch(Character::isDigit)) {
+
+			number = Integer.parseInt(request.getParameter("number"));
+		}
+		PrintWriter writer = response.getWriter();
+
+		writer.println("<html><body><table>");
+		for (int i = 1; i <= number; i++) {
+
+			writer.print("<tr><td>" + i + " * " + number + " = " + i * number + "</tr></td>");
+		}
+		writer.println("</table></html></body>");
+
+	}
+
+}
