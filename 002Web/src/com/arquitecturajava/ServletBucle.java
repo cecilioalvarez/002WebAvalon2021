7298be2973eda29afb0bc22ffc80d95a74de1eac
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-07-23 13:34:08 +0200

    servlet

---
+++ 002Web/src/com/arquitecturajava/ServletBucle.java
@@ -0,0 +1,27 @@
+package com.arquitecturajava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ServletBucle")
+public class ServletBucle extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter writer=response.getWriter();
+		writer.println("<html><body>");
+		for(int i=0;i<10;i++) {
+			writer.println("<p>"+i+"</p>");
+		}
+		writer.println("</body></html>");
+	}
+
+
+}
