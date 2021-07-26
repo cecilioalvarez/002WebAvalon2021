58df193acc10789f91349b144eb93124ab191103
RubenPS <rpozo1991@gmail.com>
2021-07-26 08:33:51 +0200

    commit html ultimo dia

---
+++ 002Web/src/com/arquitecturajava/ServletOperaciones.java
@@ -0,0 +1,36 @@
+package com.arquitecturajava;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletOperaciones
+ */
+@WebServlet("/ServletOperaciones")
+public class ServletOperaciones extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int numero1 = 0;
+		int numero2 = 0;
+		if(request.getParameter("numero1").chars().allMatch(Character::isDigit)&&request.getParameter("numero2").chars().allMatch(Character::isDigit)) {
+			numero1=Integer.parseInt(request.getParameter("numero1"));
+			numero2=Integer.parseInt(request.getParameter("numero2"));
+		}
+		
+		PrintWriter pw= response.getWriter();
+		pw.println("<html><body>");
+		pw.println("<p>" + numero1 + " + " + numero2 + " = " + (numero1+numero2) + "</p>");
+		pw.println("<p>" + numero1 + " - " + numero2 + " = " + (numero1-numero2) + "</p>");
+		pw.println("<p>" + numero1 + " * " + numero2 + " = " + (numero1*numero2) + "</p>");
+		pw.println("<p>" + numero1 + " / " + numero2 + " = " + (numero1/numero2) + "</p>");
+		pw.println("</body></html>");
+	}
+
+}
--- 002Web/src/com/arquitecturajava/ServletRadios.java
+++ 002Web/src/com/arquitecturajava/ServletRadios.java
@@ -9,22 +9,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/ServletBucleDinamico")
-public class ServletBucleDinamico extends HttpServlet {
-private static final long serialVersionUID = 1L;
+/**
+ * Servlet implementation class ServletRadios
+ */
+@WebServlet("/ServletRadios")
+public class ServletRadios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int tope = 0;
-		if(request.getParameter("numero").chars().allMatch(Character::isDigit)) {
-			tope=Integer.parseInt(request.getParameter("numero"));
-		}
-		
 		PrintWriter writer = response.getWriter();
-		writer.println("<html><body>");
-		for(int i=1; i<=tope ;i++) {
-			writer.println("<p>" + i + "</p>");
-		}
-		writer.println("</body></html>");
+		writer.println("<html><body>" + request.getParameter("lenguaje")+"</body></html>");
 	}
+
+
 }
