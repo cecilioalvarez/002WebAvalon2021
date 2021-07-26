fcd8cdf875e4d4a5f7d91a48c6c59fc92fc98b71
Molocale <molocale@gmail.com>
2021-07-26 08:49:56 +0200

    METODO POST

---
+++ 002Web/src/arquitecturajava/ServletLibro.java
@@ -0,0 +1,24 @@
+package arquitecturajava;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletLibro
+ */
+@WebServlet("/ServletLibro")
+public class ServletLibro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String isbn = request.getParameter("isbn");
+		String titulo = request.getParameter("titulo");
+		String autor = request.getParameter("autor");
+
+	}
+
+}
