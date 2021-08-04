1c452b243d0c7eaaea9f87e1ca243df75e1a7196
cecilioAlvarez <cmac@arquitecturajava.com>
2021-08-04 12:27:59 +0200

    ajax 1

---
+++ 002Web/src/com/arquitecturajava/viejo/ServletAjax.java
@@ -0,0 +1,33 @@
+package com.arquitecturajava.viejo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAjax
+ */
+@WebServlet("/ServletAjax")
+public class ServletAjax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter pw=response.getWriter();
+		pw.println("hola,que,tal,estas");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
