8baa19fdf42a8731655d4b14d6db6dc967d8a34c
RubenPS <rpozo1991@gmail.com>
2021-08-04 12:52:22 +0200

    ajax

---
+++ 002Web/src/com/arquitecturajava/viejo/ServletAjax.java
@@ -0,0 +1,28 @@
+package com.arquitecturajava.viejo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAjax
+ */
+@WebServlet("/ServletAjax")
+public class ServletAjax extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw=response.getWriter();
+		pw.println("hola, que, tal, estas");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ 002Web/src/com/arquitecturajava/viejo/ServletAjax2.java
@@ -0,0 +1,28 @@
+package com.arquitecturajava.viejo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.arquitecturajava.negocio.Libro;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Servlet implementation class ServletAjax
+ */
+@WebServlet("/ServletAjax2")
+public class ServletAjax2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Libro l = new Libro("1a","java","pedro");
+		ObjectMapper mapeador = new ObjectMapper();
+		PrintWriter pw = response.getWriter();
+		pw.print(mapeador.writeValueAsString(l));
+	}
+}
