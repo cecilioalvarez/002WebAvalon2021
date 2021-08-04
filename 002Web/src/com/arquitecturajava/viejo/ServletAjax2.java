242b1b5b91b4a579cf3ea256459e776aa3c2433c
RubenPS <rpozo1991@gmail.com>
2021-08-04 14:28:51 +0200

    Obtenemos los datos de la base de datos y los mostramos por pantalla

--- 002Web/src/com/arquitecturajava/viejo/ServletAjax2.java
+++ 002Web/src/com/arquitecturajava/viejo/ServletAjax2.java
@@ -3,6 +3,7 @@ package com.arquitecturajava.viejo;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.arquitecturajava.negocio.Libro;
+import com.arquitecturajava.repositorios.jdbc.CapituloRepositoryJDBC;
+import com.arquitecturajava.repositorios.jdbc.LibroRepositoryJDBC;
+import com.arquitecturajava.servicios.LibroService;
+import com.arquitecturajava.servicios.standard.LibroServiceStandard;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -24,8 +29,14 @@ public class ServletAjax2 extends HttpServlet {
 		Libro l = new Libro("1a","java","pedro");
 		Libro l2 = new Libro("2a","php","ana");
 		
+		/*ObjectMapper mapeador = new ObjectMapper();
+		PrintWriter pw = response.getWriter();
+		pw.print(mapeador.writeValueAsString(Arrays.asList(l,l2)));*/
+		
+		LibroService servicio = new LibroServiceStandard(new LibroRepositoryJDBC(),new CapituloRepositoryJDBC());
+		List<Libro> listaLibros = servicio.buscarTodos();
 		ObjectMapper mapeador = new ObjectMapper();
 		PrintWriter pw = response.getWriter();
-		pw.print(mapeador.writeValueAsString(Arrays.asList(l,l2)));
+		pw.print(mapeador.writeValueAsString(listaLibros));
 	}
 }
